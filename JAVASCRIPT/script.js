c26870b2114c8c4868a89e1acb1d598dcb5050d7
akshatgoswami-dev <akshatgoswami036@gmail.com>
2026-05-14 18:23:53 +0530

    learrnt some js concepts, built my first logic.

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,20 @@
+// let x = 10;
+// let y = 5;
+// if (x > y && y < x) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+
+let isadmin = 4
+let access = 12 
+if ( isadmin*3 == access || access/4 == isadmin)
+    {
+    console.log("access-granted") 
+    
+}
+ else {
+    console.log("access-denied")
+}
+
