8d02483d0ebe186f54f1519ebe672d21928927da
akshatgoswami-dev <akshatgoswami036@gmail.com>
2026-08-18 17:45:20 +0530

    feat: add task deletion and toast notification functionality in to-do list. project completed as i have reached the extent of my current knowledge

--- JAVASCRIPT/experiment-0/to-do list/script.js
+++ JAVASCRIPT/experiment-0/to-do list/script.js
@@ -4,24 +4,49 @@ let addTask = document.querySelector("#modal-add");
 let modal = document.querySelector(".modal");
 let taskInput = document.querySelector("#task-input");
 
+let form = document.querySelector("#form");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+console.log(tasks);
+function saveToLocalStorage(obj) {
+  if (localStorage.getItem("tasks") === null) {
+    let currentTasks = [];
+    currentTasks.push(obj);
+    localStorage.setItem("tasks", JSON.stringify([obj]));
+  } else {
+    let oldTasks = localStorage.getItem("tasks");
+    oldTasks = JSON.parse(oldTasks);
+    oldTasks.push(obj);
+    localStorage.setItem("tasks", JSON.stringify(oldTasks));
+  }
+}
+
+displayTasks(tasks);
 addBtn.addEventListener("click", () => {
-  modal.classList.add("modal-hide");
+  modal.classList.add("modal-show");
 });
 
 closeBtn.addEventListener("click", () => {
-  modal.classList.remove("modal-hide");
+  modal.classList.remove("modal-show");
 });
 
 addTask.addEventListener("click", (e) => {
   e.preventDefault();
+
   const task = taskInput.value;
+  if (task === "") {
+    return showToast("❌Please Enter a Task!");
+  }
   //   console.log(task);
   CreateTask(task);
+
+  tasks.push(task);
+  saveToLocalStorage(task);
+  modal.classList.remove("modal-show");
+  location.reload();
 });
 
-function CreateTask(elem) {
-  const form = document.querySelector("#form");
-
+function CreateTask(elem, index) {
   let label = document.createElement("label");
   label.classList.add("form-item");
 
@@ -29,11 +54,42 @@ function CreateTask(elem) {
   inp.classList.add("content");
   inp.type = "checkbox";
 
+  let dlt = document.createElement("button");
+  dlt.type = "button";
+  let icon = document.createElement("i");
+  icon.className = "icon";
+  icon.classList.add("ri-delete-bin-5-fill");
+
+  dlt.append(icon);
   label.append(inp);
+  dlt.addEventListener("click", (e) => {
+    console.log(`delete button clicked {${elem}}`);
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    location.reload();
+  });
   label.append(elem);
-  console.log(label);
+  label.appendChild(dlt);
 
   form.append(label);
+
+  return form;
 }
 
-function displayTasks() {}
+function displayTasks(task) {
+  form.innerHTML = "";
+  task.forEach((input, index) => {
+    CreateTask(input, index);
+  });
+}
+
+function showToast(message) {
+  const toast = document.querySelector(".toast");
+
+  toast.textContent = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2500);
+}
